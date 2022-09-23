5c2344052bb9a32aeaa43c0e6bd5bdea5df70081
Dmitriy <dkentry@yandex.ru>
2022-09-23 19:52:39 +0300

    дз 13

---
+++ dz_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    int b = int.Parse(Convert.ToString(a)[2].ToString());
+    Console.WriteLine($"третья цифра {b}");
+}
