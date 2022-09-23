bfd3f4c51e73e28f219464a7e2a7578cfb87cddd
Dmitriy <dkentry@yandex.ru>
2022-09-23 19:57:52 +0300

    дз 10

---
+++ dz_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int b = ( a / 10 ) % 10;
+if (a > 999)
+{
+    Console.WriteLine("неверное число");
+}
+else if (a < 100)
+{
+    Console.WriteLine("неверное число");
+}
+else
+{
+   Console.WriteLine($"вторая цифра {b}");
+}
